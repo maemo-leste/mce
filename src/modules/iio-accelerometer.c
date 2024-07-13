maemo-leste/mce

commit 0dac5a7e4286b5e9a4dafc627c49cef8cc92f092

accelerometer: report face up/down states
Signed-off-by: Sicelo A. Mhlongo <absicsz@gmail.com>

src/modules/iio-accelerometer.c
@@ -30,7 +30,9 @@ G_MODULE_EXPORT module_info_struct module_info = {
 typedef enum {
 	ORIENTATION_UNKNOWN,
 	ORIENTATION_LANDSCAPE,
-	ORIENTATION_PORTRAIT
+	ORIENTATION_PORTRAIT,
+	ORIENTATION_FACE_DOWN,
+	ORIENTATION_FACE_UP
 } orientation_t;
 
 static display_state_t display_state = { 0 };
@@ -59,6 +61,10 @@ static const char *iio_orientation_to_str(const orientation_t orit)
 			return MCE_ORIENTATION_LANDSCAPE;
 		case ORIENTATION_PORTRAIT: 
 			return MCE_ORIENTATION_PORTRAIT;
+		case ORIENTATION_FACE_DOWN:
+			return MCE_ORIENTATION_FACE_DOWN;
+		case ORIENTATION_FACE_UP:
+			return MCE_ORIENTATION_FACE_UP;
 		default:
 			return MCE_ORIENTATION_UNKNOWN;
 	}
@@ -68,7 +74,7 @@ static gboolean send_device_orientation(DBusMessage *const method_call)
 {
 	const gchar *srotation = iio_orientation_to_str(orientation);
 	const gchar *sstand = MCE_ORIENTATION_OFF_STAND;
-	const gchar *sface = MCE_ORIENTATION_FACE_UP;
+	const gchar *sface = (strcmp(iio_orientation_to_str(orientation), MCE_ORIENTATION_FACE_DOWN) == 0) ? MCE_ORIENTATION_FACE_DOWN : MCE_ORIENTATION_FACE_UP;
 	dbus_int32_t maxInt = G_MAXINT32;
 	DBusMessage *msg = NULL;
 	
@@ -118,6 +124,14 @@ static void iio_accel_get_value(GDBusProxy * proxy)
 	}
 	else if (strcmp(g_variant_get_string(v, NULL), "left-up") == 0) {
 		orientation = ORIENTATION_PORTRAIT;
+		changed = true;
+	}
+	else if (strcmp(g_variant_get_string(v, NULL), "face-up") == 0) {
+		orientation = ORIENTATION_FACE_UP;
+		changed = true;
+	}
+	else if (strcmp(g_variant_get_string(v, NULL), "face-down") == 0) {
+		orientation = ORIENTATION_FACE_DOWN;
 		changed = true;
 	}
 	g_variant_unref(v);

commit d08c0bd41c7028826b87253a24d264c51d228372

accelerometer: track face up/down separately from landscape/portrait
mce's dbus payload reports face up/down state separately from
landscape/portrait states. this allows, for example, device to remain in
portrait mode while laying face up/down. add support for this behavior.

Signed-off-by: Sicelo A. Mhlongo <absicsz@gmail.com>
Fixes: 0dac5a7e4286 ("accelerometer: report face up/down states")

src/modules/iio-accelerometer.c
@@ -45,6 +45,7 @@ static GDBusProxy *iio_proxy = NULL;
 static GSList *accelerometer_listeners = NULL;
 
 static orientation_t orientation = ORIENTATION_UNKNOWN;
+static orientation_t horizontal = ORIENTATION_FACE_UP;
 
 static bool iio_accel_claim_policy(void)
 {
@@ -74,7 +75,7 @@ static gboolean send_device_orientation(DBusMessage *const method_call)
 {
 	const gchar *srotation = iio_orientation_to_str(orientation);
 	const gchar *sstand = MCE_ORIENTATION_OFF_STAND;
-	const gchar *sface = (strcmp(iio_orientation_to_str(orientation), MCE_ORIENTATION_FACE_DOWN) == 0) ? MCE_ORIENTATION_FACE_DOWN : MCE_ORIENTATION_FACE_UP;
+	const gchar *sface = iio_orientation_to_str(horizontal);
 	dbus_int32_t maxInt = G_MAXINT32;
 	DBusMessage *msg = NULL;
 	
@@ -127,17 +128,17 @@ static void iio_accel_get_value(GDBusProxy * proxy)
 		changed = true;
 	}
 	else if (strcmp(g_variant_get_string(v, NULL), "face-up") == 0) {
-		orientation = ORIENTATION_FACE_UP;
+		horizontal = ORIENTATION_FACE_UP;
 		changed = true;
 	}
 	else if (strcmp(g_variant_get_string(v, NULL), "face-down") == 0) {
-		orientation = ORIENTATION_FACE_DOWN;
+		horizontal = ORIENTATION_FACE_DOWN;
 		changed = true;
 	}
 	g_variant_unref(v);
 	
 	if (changed) {
-		mce_log(LL_DEBUG, "%s: orientation: %s", MODULE_NAME, iio_orientation_to_str(orientation));
+		mce_log(LL_DEBUG, "%s: orientation: %s, horizontal orientation: %s", MODULE_NAME, iio_orientation_to_str(orientation), iio_orientation_to_str(horizontal));
 		send_device_orientation(NULL);
 	}
 }